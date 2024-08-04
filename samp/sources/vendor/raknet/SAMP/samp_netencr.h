9470aa0e4d40c6e52c29eecde488f228b04762b3
PeriaCyber <124803329+PeriaCyber@users.noreply.github.com>
2024-08-04 20:36:49 +0700

    Delete samp/sources/vendor/raknet/SAMP directory

--- samp/sources/vendor/raknet/SAMP/samp_netencr.h
+++
@@ -1,7 +0,0 @@
-// https://github.com/P3ti/RakSAMP/blob/master/raknet/SAMP/samp_netencr.h
-
-/*
-	Updated to 0.3.7 by P3ti
-*/
-extern unsigned char encrBuffer[4092];
-void kyretardizeDatagram(unsigned char *buf, int len, int port, int unk);
